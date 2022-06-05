ec14f844bbb5ba24b90a7beffc730f2c46124794
Crowdbotics <team@crowdbotics.com>
2022-06-05 15:44:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nurkey_fashion_35599",
+  url: "https://nurkey_fashion_35599.botics.co",
+  api: "https://nurkey_fashion_35599.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
